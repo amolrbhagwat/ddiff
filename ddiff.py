ef6ad686583ea9fa6cce4dee0b8789510d767177
Amol Bhagwat <amolrbhagwat@gmail.com>
2019-04-07 17:45:03 -0400

    Add file matching algorithm

--- ddiff.py
+++ ddiff.py
@@ -24,6 +24,42 @@ def prompt_directory(title):
     currdir = os.getcwd()
     return filedialog.askdirectory(parent=root, initialdir=currdir, title=title)
 
+def create_index(directory):
+    filename_dirs_map = {}
+    for dirname, dirnames, filenames in os.walk(directory):
+        for filename in filenames:
+            if filename not in filename_dirs_map:
+                filename_dirs_map[filename] = []
+            filename_dirs_map[filename].append(dirname)
+
+    return filename_dirs_map
+
+def create_list_of_src_files(directory):
+    file_list = []
+    for dirname, dirnames, filenames in os.walk(directory):
+        for filename in filenames:
+            file_list.append(filename)
+
+    return file_list
+
+def ddiff():
+    global source_dir, target_dir
+    total_found = 0
+    total_missing = 0
+
+    index = create_index(target_dir)
+    src_list = create_list_of_src_files(source_dir)
+
+    for filename in src_list:
+        if filename in index:
+            total_found += 1
+            print(filename, "is present in the target directory under", index[filename])
+        else:
+            total_missing += 1
+            print(filename, "IS MISSING FROM TARGET DIRECTORY!")
+
+    print("Total found:", total_found)
+    print("Total missing:", total_missing)
 
 def main():
     global source_dir, target_dir
@@ -35,6 +71,8 @@ def main():
             source_dir = prompt_directory('Select source directory')
         if option == "2":
             target_dir = prompt_directory('Select target directory')
+        if option == "3":
+            ddiff()
         if option == "4":
             sys.exit(0)
 
