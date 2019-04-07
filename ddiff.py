0db8bd0045bed8c1a6ce39b3ec1638905e58b918
Amol Bhagwat <amolrbhagwat@gmail.com>
2019-04-07 17:18:41 -0400

    Changes to directory prompt
    Move tkinter initialization out of function
    Add titles to dir selection window

--- ddiff.py
+++ ddiff.py
@@ -3,6 +3,10 @@ import tkinter.filedialog as filedialog
 import os
 import sys
 
+root = Tkinter.Tk()
+root.withdraw() #use to hide tkinter window
+root.wm_attributes('-topmost', 1)
+
 source_dir = ''
 target_dir = ''
 
@@ -14,15 +18,12 @@ def show_main_menu():
     print("3. Check if target contains files under source")
     print("4. Exit")
 
-def prompt_directory():
-    root = Tkinter.Tk()
-    root.withdraw() #use to hide tkinter window
-    root.wm_attributes('-topmost', 1)
+def prompt_directory(title):
+    global root
 
     currdir = os.getcwd()
-    tempdir = filedialog.askdirectory(parent=root, initialdir=currdir, title='Please select a directory')
+    return filedialog.askdirectory(parent=root, initialdir=currdir, title=title)
 
-    return tempdir
 
 def main():
     global source_dir, target_dir
@@ -31,9 +32,9 @@ def main():
         show_main_menu()
         option = input("Enter an option: ")
         if option == "1":
-            source_dir = prompt_directory()
+            source_dir = prompt_directory('Select source directory')
         if option == "2":
-            target_dir = prompt_directory()
+            target_dir = prompt_directory('Select target directory')
         if option == "4":
             sys.exit(0)
 
